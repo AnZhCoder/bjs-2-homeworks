d82668682ed3119daa78e72306d7312953a44b1e
Andrey Zhukov <whitefox25vl@gmail.com>
2022-08-12 20:02:35 +1200

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+	"use strict";
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+			arr[0] = Math.ceil(-b/(2*a));
+		return arr
+	} else if (d > 0) {
+		arr[0] = Math.ceil((-b + Math.sqrt(d)) / (2 * a)); 
+		arr[1] = Math.ceil((-b - Math.sqrt(d)) / (2 * a));
+		return arr;
+	}
+
+	return arr; // array
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+	let totalAmount;
 
-  // код для задачи №2 писать здесь
+	 //код для задачи №2 писать здесь
 
-  return totalAmount;
+	return totalAmount;
 }
