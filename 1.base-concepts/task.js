4451c3b3ff3486ec6946c92c640061fb726faa50
Andrey Zhukov <whitefox25vl@gmail.com>
2022-08-13 17:01:15 +1200

    edited task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,45 @@
+"use strict";
 
 function solveEquation(a, b, c) {
-	"use strict";
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
 
-	if (d < 0) {
-		return arr;
-	} else if (d === 0) {
-			arr[0] = Math.ceil(-b/(2*a));
-		return arr
+	if (d === 0) {
+		arr[0] = -b / (2 * a);
 	} else if (d > 0) {
-		arr[0] = Math.ceil((-b + Math.sqrt(d)) / (2 * a)); 
-		arr[1] = Math.ceil((-b - Math.sqrt(d)) / (2 * a));
-		return arr;
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
 	}
 
-	return arr; // array
+	return arr;
 }
 
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-	let totalAmount;
+	
+	if (typeof percent !== 'number') {
+		return `'Параметр "Процентная ставка" содержит неправильное значение ${percent}'`;
+	};
 
-	 //код для задачи №2 писать здесь
+	if (typeof contribution !== 'number') {
+		return `'Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}'`;
+	};
 
-	return totalAmount;
+	if (typeof amount !== 'number') {
+		return `'Параметр "Сумма кредита" содержит неправильное значение ${amount}'`;
+	};
+
+	let payuotAmount = Number(amount) - Number(contribution);
+	let currentYear = new Date();
+	let totalMonths = (date.getFullYear() - currentYear.getFullYear()) * 12;
+	let p = (Number(percent) / 12) / 100;
+	let monthlyPayment = payuotAmount * (p + (p / (Math.pow(1 + p, totalMonths) - 1)));
+	let totalAmount = monthlyPayment * totalMonths;
+
+
+
+	console.log(Number(totalAmount.toFixed(2)));
+
+	return Number(totalAmount.toFixed(2));
 }
