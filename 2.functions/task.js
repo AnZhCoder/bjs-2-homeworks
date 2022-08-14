9cf5789939793ee9094a02592cae70912682005f
Andrey Zhukov <whitefox25vl@gmail.com>
2022-08-14 19:22:33 +1200

    edited task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,66 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += 1;
+    avg = avg + arr[i];
+  }
+
+  avg = Number((avg / sum).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sumElements = 0;
 
-  // Ваш код
-
-  return sum;
+  for (let i in arr) {
+    sumElements += arr[i];
+  }
+  return sumElements;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  func();
 
-  // Ваш кода
-  // for ...
-  
+  for (let i in arrOfArr) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let minElement = Infinity;
+  let maxElement = -Infinity;
+
+  for (let i in arr) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+
+    if (arr[i] < minElement) {
+      minElement = arr[i];
+    }
+  }
+
+  let difference = maxElement - minElement;
+  return Math.abs(difference);
 }
