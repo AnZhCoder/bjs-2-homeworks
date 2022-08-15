28f923269045bdad5f7b5ebeaad08a9b8a89127e
Andrey Zhukov <whitefox25vl@gmail.com>
2022-08-15 19:19:36 +1200

    fix task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@
 
 function compareArrays(arr1, arr2) {
 	let result = arr1.every((item, index) => arr2[index] === item);
-	
+
 	if (arr1.length !== arr2.length) {
 		return false;
 	}
@@ -13,12 +13,9 @@ function compareArrays(arr1, arr2) {
 //Задание № 2
 
 function advancedFilter(arr) {
-  let resultArr;
+	let resultArr;
 
-  resultArr = arr.filter((item) => item > 0 && item % 3 ===0).map((item) => item * 10);
+	resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
 
-  return resultArr;
+	return resultArr;
 }
-
-
-//.filter((item) => resultArr[item] % 3 == 0).map((item) => resultArr[item] * 10)
