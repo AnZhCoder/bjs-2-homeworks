fb22c4f9d47101b16a5125c59731de4beb237937
Andrey Zhukov <whitefox25vl@gmail.com>
2022-08-20 18:23:12 +1200

    edited task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
+
+const firstStudent = new Student('Vanessa', 'female', 25);
+const secondStudent = new Student('Gregory', 'male', 36);
+const thirtyStudent = new Student('Bob', 'male', 30);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+	this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+	if (this.marks === undefined) {
+		this.marks = [mark];
+	} else {
+		this.marks.push(mark);
+	}
+};
+
+Student.prototype.addMarks = function (...mark) {
+	if (this.marks === undefined) {
+		this.marks = mark;
+	} else {
+		this.marks.push(...mark);
+	}
+};
+
+Student.prototype.getAverage = function () {
+	return this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0);
+};
+
+Student.prototype.exclude = function (reason) {
+	delete this.subject && delete this.marks;
+	this.excluded = reason;
+};
